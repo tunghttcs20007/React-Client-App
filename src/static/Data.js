828ffe9f227d1d752428fd40a2afb8dc904f7806
tung.huynh <tunghttcs2007@fpt.edu.vn>
2022-11-21 15:55:24 +0700

    Fix: add more brand

--- src/static/Data.js
+++ src/static/Data.js
@@ -1,5 +1,5 @@
 export const COLOR = ['Black', 'Brown', 'Silver', 'White', 'Blue'];
-export const BRAND = ['Apple', 'Samsung', 'Microsoft', 'Lenovo', 'Asus'];
+export const BRAND = ['Apple', 'HP', 'Microsoft', 'Lenovo', 'Asus', 'Dell'];
 export const SHIPPING = ['Yes', 'No'];
 export const ORDER_STATUS = [
 	'NOT PROCESSED',
